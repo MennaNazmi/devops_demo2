358a12b871d0e04dfbc17103b6bd78a053611f2e
mennaNazmi <mennanazmii@gmail.com>
2023-01-02 23:53:41 +0200

    sec

--- b_scripts.sh
+++ b_scripts.sh
@@ -4,3 +4,6 @@ echo "Hi,Menna! "
 # What to backup.
 backup_files="/home/menna/obs_task/"
 # Where to backup to.
+dest="ubuntu@34.205.172.213:/home/ubuntu/files_backup/"
+
+rsync -av   $backup_files  -e "ssh -i ans_try_book.pem"  $dest | grep -w "f[0-9]\|f1[0-5]"
